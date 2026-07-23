f4f7ad49b7ccbfbeff60648024cd191cd9b254c9
TestMoi75016 <159545160+TestMoi75016@users.noreply.github.com>
2026-07-23 21:03:57 +0200

    Ajoute la connexion à la base SQLite Closes #3 (#21)
    Co-authored-by: Mike Dev <george.etudiants@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.database import creer_tables
 
-app = FastAPI(title="Gestion de stock")
+
+@asynccontextmanager  # décorateur
+async def lifespan(app: FastAPI):
+    # Avant le yield : au démarrage du serveur
+    creer_tables()
+    yield
+    # Après le yield : à l'extinction du serveur (rien à faire ici)
+
+
+app = FastAPI(title="Gestion de stock", lifespan=lifespan)
 
 
 @app.get("/")
