4451d7925ae5bdb922da7256375c7c9ea49da9b5
Mike Dev <george.etudiants@gmail.com>
2026-07-27 18:00:48 +0200

    Produit: modifier et supprimer
    Closes #5

--- app/routes/produits.py
+++ app/routes/produits.py
@@ -23,10 +23,37 @@ def lister_produits(session: Session = Depends(get_session)):
     return produits
 
 
-# ---route lister un produit en particulier-
+# ---route lister un produit en particulier -
 @router.get("/{produit_id}", response_model=Produit)
 def lire_produit(produit_id: int, session: Session = Depends(get_session)):
     produit = session.get(Produit, produit_id)
     if produit is None:
         raise HTTPException(status_code=404, detail="Produit introuvable")
     return produit
+
+
+# ---route modifier un produit en particulier -
+@router.put("/{produit_id}", response_model=Produit)
+def modifier_produit(
+    produit_id: int, produit_maj: Produit, session: Session = Depends(get_session)
+):
+    produit = session.get(Produit, produit_id)
+    if produit is None:
+        raise HTTPException(status_code=404, detail="Produit introuvable")
+    donnees = produit_maj.model_dump(exclude_unset=True)
+    produit.sqlmodel_update(donnees)
+    session.add(produit)
+    session.commit()
+    session.refresh(produit)
+    return produit
+
+
+# ---route supprimer un produit en particulier -
+@router.delete("/{produit_id}")
+def supprimer_produit(produit_id: int, session: Session = Depends(get_session)):
+    produit = session.get(Produit, produit_id)
+    if produit is None:
+        raise HTTPException(status_code=404, detail="Produit introuvable")
+    session.delete(produit)
+    session.commit()
+    return {"ok": True}
