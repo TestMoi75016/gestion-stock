bd2d7568c23ebcffa6d5f81c331b71bb6627b42c
Mike Dev <george.etudiants@gmail.com>
2026-08-09 03:31:48 +0200

    Ticket #11 (Partie 2) : Mouvements sur une periode
    Closes #11

--- app/routes/rapports.py
+++ app/routes/rapports.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import case, func
 from sqlmodel import Session, select
 
@@ -22,3 +24,24 @@ def lire_valeur_stock(session: Session = Depends(get_session)):
     )
     resultat = session.exec(statement).one()
     return {"valeur_totale": resultat}
+
+
+# ---route lister les mouvements sur une période -
+@router.get("/mouvements")
+def lire_mouvements(
+    debut: datetime, fin: datetime, session: Session = Depends(get_session)
+):
+    if debut > fin:
+        raise HTTPException(
+            status_code=400,
+            detail="La date de début doit être antérieure ou égale à la date de fin.",
+        )
+    statement = (
+        select(
+            Mouvement
+        )  # Mouvement (majuscule) = la classe (le moule), importée en haut du fichier : from app.models import Mouvement
+        .where(Mouvement.date >= debut, Mouvement.date <= fin)
+        .order_by(Mouvement.date)
+    )
+    mouvements = session.exec(statement).all()
+    return mouvements
